f2a1f0c87b99a83d03dc908f8fef73565e8edb80
Matt Weaver <Matt.Weaver@bbc.co.uk>
2014-08-27 18:50:08 +0100

    Adds initial test of combined mocking.

--- test/calculator-squire-spec.js
+++ test/calculator-squire-spec.js
@@ -4,11 +4,13 @@ define([
 
     var injector = new Squire();
 
-    injector.mock('src/math', {
+    var mathMock = {
         addNumbers: function (a, b) {
             return a - b;
         }
-    });
+    };
+
+    injector.mock('src/math', mathMock);
 
     describe('testing with the mock math library in the calculator', function () {
 
@@ -28,6 +30,36 @@ define([
                 expect(Calculator.add(1,2)).toBe(3);
                 done();
             });
+        });
+
+    });
+
+    describe('testing with a combination of the real and mock math library in the calculator', function () {
+
+        it('should add numbers together using the mock Math library', function (done) {
+             injector.require(['src/calculator'], function(Calculator) {
+                expect(Calculator.add(1,2)).toBe(-1);
+                done();
+            });
+        });
+
+        it('should multiply numbers together using the real Math library', function (done) {
+
+            var fakeMath;
+
+            require(['src/math'], function(Math) {
+                fakeMath = $.extend(true, {}, Math, mathMock);
+
+                var injector2 = new Squire();
+                injector2.mock('src/math', fakeMath);
+
+                injector2.require(['src/calculator'], function(Calculator) {
+                    expect(Calculator.add(9,3)).toBe(6);
+                    expect(Calculator.times(9,9)).toBe(81);
+                    done();
+                });
+            });
+
         });
 
     });
