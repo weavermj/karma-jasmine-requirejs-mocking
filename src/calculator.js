d18e4a3f6f56beb6e51951df4acdb64318f177b4
Matt Weaver <Matt.Weaver@bbc.co.uk>
2014-08-27 16:00:38 +0100

    Added new functions to libraries

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,9 @@ define('src/calculator', ['src/math'], function (Math) {
     return {
         add: function (a,b) {
             return Math.addNumbers(a,b);
+        },
+        times: function (a,b) {
+            return Math.multiplyNumbers(a,b);
         }
     };
 });
--- src/math.js
+++ src/math.js
@@ -2,6 +2,9 @@ define('src/math', [], function () {
     return {
         addNumbers: function (a,b) {
             return a + b;
+        },
+        multiplyNumbers: function (a,b) {
+            return a * b;
         }
     };
 });
