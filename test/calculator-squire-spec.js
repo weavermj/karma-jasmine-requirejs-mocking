dea5b0adeeff0d05f84bbb8a5363948eab5cd7b9
Matt Weaver <Matt.Weaver@bbc.co.uk>
2014-08-27 21:51:39 +0100

    Drys up the tests. Adds some comments.

--- test/calculator-squire-spec.js
+++ test/calculator-squire-spec.js
@@ -3,17 +3,23 @@ define([
 ], function (Squire) {
 
     var injector = new Squire();
-    var injector2 = new Squire();
 
     var mathMock = {
-        addNumbers: function (a, b) {
-            return a - b;
-        }
-    };
-
-    injector.mock('src/math', mathMock);
+            addNumbers: function (a, b) {
+                return a - b;
+            }
+        };
 
     describe('testing with the mock math library in the calculator', function () {
+
+        beforeEach(function () {
+            // set the mock to the mathMock
+            injector.mock('src/math', mathMock);
+        });
+
+        afterEach(function(){
+            injector.clean('src/math');
+        });
 
         it('should add numbers together using the mock Math library', function (done) {
             injector.require(['src/calculator'], function(Calculator) {
@@ -37,7 +43,11 @@ define([
 
     describe('testing with a combination of the real and mock math library in the calculator', function () {
 
-        var fakeMath;
+        // here we want to override the addNumbers behaviour to actually subtracts numbers,
+        // but keep the existing multiplyNumbers behaviour
+
+        var fakeMath,
+            injector;
 
         beforeEach(function (done) {
             // require the real math module
@@ -45,68 +55,30 @@ define([
                 // extend the math module with the math mock
                 // any existing functions are overwritten
                 fakeMath = $.extend(true, {}, Math, mathMock);
+                injector = new Squire();
+
+                // set the fakeMath module as the math mock
+                injector.mock('src/math', fakeMath);
                 done();
             });
+
         });
 
         afterEach(function(){
             injector.clean('src/math');
         });
 
-
         it('should multiply numbers together using the real Math library function', function (done) {
-            var injector = new Squire();
-
-            injector.mock('src/math', fakeMath);
             injector.require(['src/calculator'], function(Calculator) {
                 expect(Calculator.times(9,9)).toBe(81);
                 done();
             });
-
         });
 
-         it('should add numbers together using the mock Math library function', function (done) {
-            var injector = new Squire();
-
-            injector.mock('src/math', fakeMath);
+        it('should add numbers together using the mock Math library function', function (done) {
             injector.require(['src/calculator'], function(Calculator) {
                 expect(Calculator.add(1,2)).toBe(-1);
                 done();
-            });
-
-        });
-
-        xit('should multiply numbers together using the real Math library function', function (done) {
-            var fakeMath;
-
-            require(['src/math'], function(Math) {
-                fakeMath = $.extend(true, {}, Math, mathMock);
-
-                var injector = new Squire();
-                injector.mock('src/math', fakeMath);
-
-                injector.require(['src/calculator'], function(Calculator) {
-                    expect(Calculator.times(9,9)).toBe(81);
-                    done();
-                });
-            });
-
-        });
-
-        xit('should add numbers together using the mock Math library function', function (done) {
-            var fakeMath;
-
-            require(['src/math'], function(Math) {
-                fakeMath = $.extend(true, {}, Math, mathMock);
-
-                injector = new Squire();
-                injector.mock('src/math', fakeMath);
-
-                injector.require(['src/calculator'], function(Calculator) {
-                    expect(Calculator.add(9,3)).toBe(6);
-                    done();
-                });
-
             });
         });
 
